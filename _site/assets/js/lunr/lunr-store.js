27fecd93c225ffc28e2013640b9351da7dd89f02
eric dong <dong0085@algonquinlive.com>
2026-03-02 18:59:00 -0500

    style(css): update responsive text styles. Slightly smaller texts on big screens

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,6 +5,18 @@ var store = [{
         "url": "/2025/11/01/hello-world.html",
         "teaser": null
       },{
+        "title": "The AI Gap: Why 2026 is the Year Android Left iOS Behind",
+        "excerpt":"The mobile landscape of 2026 is being reshaped by a singular force: On-Device Multimodal Generative AI. This technology is not merely a feature upgrade; it is the engine driving the most significant shift in user interaction since the introduction of the multitouch screen. However, a survey of the current market...","categories": [],
+        "tags": ["blog"],
+        "url": "/2026/02/07/Developing-for-the-Era-of-On-Device-AI.html",
+        "teaser": null
+      },{
+        "title": "Auracst: transforming public audio sharing",
+        "excerpt":"Around May 2025, Auracast quietly became part of the Android 16 along with the shipment of the latest Android OS from Google. If you aren’t familiar with this technology or how it could shape up the audio sharing concept, you might be in for a surprise of how powerful it...","categories": [],
+        "tags": ["blog"],
+        "url": "/2026/03/02/Developing-for-the-Era-of-On-Device-AI-copy.html",
+        "teaser": null
+      },{
         "title": "Weatherly: A Flutter Weather App",
         "excerpt":"Overview Weatherly is a simple but polished weather app built with Flutter. It displays current conditions, hourly and 7‑day forecasts, and supports light/dark themes. This project is a solid template for API‑driven mobile apps with clean architecture and testable components. Tech Stack Flutter (3.x) and Dart HTTP for networking Riverpod...","categories": [],
         "tags": ["flutter","dart","mobile","api","demo"],
